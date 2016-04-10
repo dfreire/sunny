d99718d3a135d70d54704ecf721c0f9079828c15
dfreire <dario.freire@gmail.com>
2016-04-10 18:27:26 +0100

    update

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/fasthttp"
 	log "github.com/mgutz/logxi/v1"
 	"github.com/spf13/viper"
 )
 
 func init() {
+	initViper()
+}
+
+func initViper() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 	viper.AutomaticEnv()
@@ -17,4 +25,13 @@ func init() {
 }
 
 func main() {
+	e := echo.New()
+
+	// e.SetDebug(true)
+
+	e.Get("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Run(fasthttp.New(":1323"))
 }
