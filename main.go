d4df7e9aea320e17141747021f0b0a5ecd8a39dc
dfreire <dario.freire@gmail.com>
2016-04-10 15:47:32 +0100

    update

--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/rifflock/lfshook"
 	"github.com/spf13/viper"
 )
 
-func main() {
+func init() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
 
 	err := viper.ReadInConfig()
 	if err != nil {
 		log.Fatal(fmt.Errorf("Couldn't read the config file"))
 	}
+
+	log.AddHook(lfshook.NewHook(lfshook.PathMap{
+		log.DebugLevel: "./log/log.txt",
+		// log.InfoLevel:  "./log/log.txt",
+		// log.WarnLevel:  "./log/log.txt",
+		// log.ErrorLevel: "./log/log.txt",
+		// log.FatalLevel: "./log/log.txt",
+		// log.PanicLevel: "./log/log.txt",
+	}))
+
+	log.SetFormatter(&log.JSONFormatter{})
+	// log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	// log.SetFormatter(&log.TextFormatter{})
+
+	// hook, err := logrus_syslog.NewSyslogHook("", "", syslog.LOG_INFO, "")
+	// if err == nil {
+	// 	log.AddHook(hook)
+	// }
+
+	log.WithFields(log.Fields{"SUNNY_ENV": viper.Get("SUNNY_ENV")}).Info("initialized")
+}
+
+func main() {
 }
