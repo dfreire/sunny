202df081bca7d84f88a90900b702ae51c2d27337
dfreire <dario.freire@gmail.com>
2016-05-23 20:19:53 +0100

    update

--- handlers/send-to-newsletter.go
+++ handlers/send-to-newsletter.go
@@ -1,15 +1,17 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/dfreire/sunny/middleware"
 	"github.com/dfreire/sunny/model"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
+	"github.com/tealeg/xlsx"
 )
 
-// http POST http://localhost:3500/send-to-newsleter
+// http POST http://localhost:3500/send-to-newsletter
 func SendToNewsletter(c echo.Context) error {
 	tx := c.Get(middleware.TX).(*gorm.DB)
 	// m := c.Get(middleware.MAILER).(mailer.Mailer)
@@ -32,4 +34,24 @@ func SendToNewsletter(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, jsonResponse{Ok: true, Result: customers})
+}
+
+func exportToExcel(customers []model.Customer) {
+	file := xlsx.NewFile()
+	sheet, err := file.AddSheet("Sheet1")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	for _, customer := range customers {
+		row := sheet.AddRow()
+
+		cell := row.AddCell()
+		cell.Value = customer.Email
+	}
+
+	err = file.Save("file.xlsx")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
 }
