582e7a81cd272baa3537fc5341b83cf9b7a1378a
dfreire <dario.freire@gmail.com>
2016-05-29 21:33:26 +0100

    update

--- handlers/sign-up-customer-with-newsletter_test.go
+++ handlers/sign-up-customer-with-newsletter_test.go
@@ -3,9 +3,27 @@ package handlers
 import (
 	"testing"
 
+	"github.com/dfreire/sunny/middleware"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
+type mockedContext struct {
+	mock.Mock
+}
+
+func (m *mockedContext) Get(key string) interface{} {
+	args := m.Called(key)
+	return args.Get(0)
+}
+
 func TestSignupCustomerWithNewsletter(t *testing.T) {
-	assert.Nil(t, nil)
+	c := new(mockedContext)
+
+	db, err := gorm.Open("sqlite3", ":memory:")
+	assert.Nil(t, err)
+	tx := db.Begin()
+	c.On("Get", middleware.TX).Return(tx)
 }
