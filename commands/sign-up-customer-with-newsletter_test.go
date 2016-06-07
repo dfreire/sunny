3f59fe4b8cc8fdda41e5fb7673f647c979fbeecc
dfreire <dario.freire@gmail.com>
2016-06-07 21:30:42 +0100

    update

---
+++ commands/sign-up-customer-with-newsletter_test.go
@@ -0,0 +1,40 @@
+package commands_test
+
+import (
+	"testing"
+
+	"github.com/dfreire/sunny/commands"
+	"github.com/dfreire/sunny/mailer"
+	"github.com/dfreire/sunny/model"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	viper.SetEnvPrefix("SUNNY")
+	viper.AutomaticEnv()
+}
+
+func TestSignupCustomerWithNewsletter(t *testing.T) {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+
+	model.Initialize(db)
+
+	tx := db.Begin()
+
+	mx := mailer.NewLogMailer()
+
+	reqData := commands.SignupCustomerWithNewsletterRequestData{
+		Name:       "Joe Doe",
+		Email:      "joe.doe@mailinator.com",
+		RoleId:     "wine_lover",
+		LanguageId: "pt",
+	}
+
+	assert.Nil(t, commands.SignupCustomerWithNewsletter(tx, mx, reqData))
+}
