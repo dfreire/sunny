bd4921848dcb335ccc42cc3f8ded6a232096bfc9
Dario Freire <dario.freire@gmail.com>
2016-07-04 22:20:44 +0100

    update

--- dist/send-contacts-to-newsletter.sh
+++ dist/send-contacts-to-newsletter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-source env.production.sh
+export HERE=`cd -P $(dirname $0) && pwd`
+source $HERE/env.production.sh
 URL="http://localhost$SUNNY_PORT/send-contacts-to-newsletter?appToken=$SUNNY_APP_TOKEN"
 curl -XPOST $URL &> /dev/null
